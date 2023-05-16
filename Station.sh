fc42b28c9dd323d630cf3581a76155ebf06cebee
Chris-tian-2000 <133801241+Chris-tian-2000@users.noreply.github.com>
2023-05-16 10:32:10 -0700

    Create Station.sh

---
+++ Station.sh
@@ -0,0 +1,57 @@
+#!bin/bash
+
+echo "Welcome to your new shipping station!"
+sleep 2
+
+echo "Installing some essentials - Stand by."
+sleep 2
+
+sudo apt update 
+sudo apt full-upgrade
+sudo apt-get install network-manager
+sudo apt-get install xdotool
+
+#Install and disable the firewall.
+sudo apt install ufw
+sudo ufw disable
+
+sudo apt install chromium-browser -y
+echo "Chromium is now installed!"
+sleep 2
+
+echo "Downloading Teamviewer now.."
+sleep 2
+wget  https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
+
+#Download and set background
+wget -O image.jpg "https://images.teamtailor-cdn.com/images/s3/teamtailor-production/landscape_16_9-v3/image_uploads/db21535e-df8c-41d6-ba34-976a3b860bfb/ori>
+
+pcmanfm --set-wallpaper="/home/picanova/image.jpg"
+
+echo "Is this a check-in/out station? (y/n)"
+read response
+
+while true; do
+    if [[ "$response" =~ ^[Yy]$ ]]; then
+        source checkin-out.sh
+        break
+    elif [[ "$response" =~ ^[Nn]$ ]]; then
+        break
+    else
+echo "Invalid input. Please enter y or n."
+        read response
+    fi
+done
+
+#The defualt name is WS-RPI-(LOCATION-NUMBER)
+echo "Hostname:"
+read hostname
+sudo sed -i 's/raspberrypi/'$hostname'/g' /etc/hosts
+sudo sed -i 's/raspberrypi/'$hostname'/g' /etc/hostname
+
+echo "This script only runs once, it will be deleted now."
+sleep 1
+#sudo rm station.sh
+
+echo "Rebooting.."
+systemctl reboot -i 
