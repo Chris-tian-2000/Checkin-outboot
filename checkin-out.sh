784ee3312f539ca74e6d532fd398ebd053e95213
Chris-tian-2000 <133801241+Chris-tian-2000@users.noreply.github.com>
2023-05-31 12:29:22 -0700

    Update checkin-out.sh

--- checkin-out.sh
+++ checkin-out.sh
@@ -3,11 +3,7 @@
 echo "Welcome to your new Checkin-out Station"
 sleep 1
 
-#The default layout is CHECK-IN-OUT-(LOCATION)
-echo "Hostname:"
-read hostname
-sudo sed -i 's/raspberrypi/'$hostname'/g' /etc/hosts
-sudo sed -i 's/raspberrypi/'$hostname'/g' /etc/hostname
+sudo mv /home/picanova/Tempauto/checkin-startup ~/.config/autostart
  
 #sudo rm checkin-out.sh
 
