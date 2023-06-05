981a902a14b6dbcb94c311ae26d63c43e584172f
Chris-tian-2000 <133801241+Chris-tian-2000@users.noreply.github.com>
2023-06-05 09:32:05 -0700

    Update checkin-out.sh

--- checkin-out.sh
+++ checkin-out.sh
@@ -3,9 +3,7 @@
 echo "Welcome to your new Checkin-out Station"
 sleep 1
 
+sudo mv /home/picanova/Desktop/picanova /etc
+sleep 1
 sudo mv /home/picanova/Tempauto/checkin-startup ~/.config/autostart
- 
-#sudo rm checkin-out.sh
-
-echo "Rebooting.."
-reboot
+sleep 1
